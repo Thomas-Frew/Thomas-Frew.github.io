d2208efaf20ee87e39066b065649574f09f14b00
Thomas-Frew <thomasfrew13@gmail.com>
2023-01-21 21:22:50 +1030

    Website rebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://thomas-frew.github.io/',
-  base: '/portfolio-website'
+  site: 'https://thomas-frew.github.io/'
 })
